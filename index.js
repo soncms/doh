6c885a91753136a4f0dac170c49c78d41fba597e
Son Ta Thanh <87874954+soncms@users.noreply.github.com>
2022-12-11 11:34:41 +0700

    Update index.js
    change dns server

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ addEventListener('fetch', function(event) {
     event.respondWith(response)
 })
 
-const doh = 'https://security.cloudflare-dns.com/dns-query'
-const dohjson = 'https://security.cloudflare-dns.com/dns-query'
+const doh = 'https://3oacheir35.cloudflare-gateway.com/dns-query'
+const dohjson = 'https://3oacheir35.cloudflare-gateway.com/dns-query'
 const contype = 'application/dns-message'
 const jstontype = 'application/dns-json'
 
