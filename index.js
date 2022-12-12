1a2c829153c48f57edef40039c75ddc918eaec37
Son Ta Thanh <87874954+soncms@users.noreply.github.com>
2022-12-12 10:49:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,8 @@ addEventListener('fetch', function(event) {
     event.respondWith(response)
 })
 
-const doh = 'https://dns.google/dns-query'
-const dohjson = 'https://dns.google/dns-query'
 const doh = 'https://dns.opendns.com/dns-query'
 const dohjson = 'https://dns.opendns.com/dns-query'
-
 const contype = 'application/dns-message'
 const jstontype = 'application/dns-json'
 
